342bdf1338c0396499fcc2a75e98f51dfa8aa480
ErinH <erhurley@csumb.edu>
2026-03-16 22:24:23 -0700

    5.2

--- Homework2/js/script.js
+++ Homework2/js/script.js
@@ -1,7 +1,31 @@
+// Event Listeners
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
+// Global variables
+
+// Functions
+function isFormValid(){
+    let isValid = true;
+
+    if(document.querySelector("#q1").value == ""){
+        isValid = false;
+        document.querySelector("#validationFdbk").innerHTML = "Question 1 was not answered";
+    }
+
+    return isValid;
+} // isFormValid
+
 function gradeQuiz(){
- console.log("Grading quiz…");
- let q1Response = document.querySelector("#q1").value;
- console.log(q1Response);
-}
+    console.log("Grading quiz..");
+
+    // resets validation feedback
+    document.querySelector("#validationFdbk").innerHTML = "";
+
+    if (!isFormValid()) {
+        return;
+    }
+
+    let q1Response = document.querySelector("#q1").value;
+    console.log(q1Response);
+
+} // gradeQuiz
