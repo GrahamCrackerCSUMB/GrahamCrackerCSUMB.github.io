2179262b44035a025251ca110ca8b91bd7af4315
ErinH <erhurley@csumb.edu>
2026-03-16 22:31:38 -0700

    section 6

--- Homework2/js/script.js
+++ Homework2/js/script.js
@@ -2,6 +2,7 @@
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
 // Global variables
+$("#totalScore").html(`Total Score: ${score}`);
 
 // Functions
 function isFormValid(){
@@ -25,7 +26,22 @@ function gradeQuiz(){
         return;
     }
 
-    let q1Response = document.querySelector("#q1").value;
+    // variables
+    let score = 0;
+    let q1Response = document.querySelector("#q1").value.toLowerCase();
     console.log(q1Response);
 
+    // Grading question 1
+    if (q1Response == "sacramento") {
+        document.querySelector("#q1Feedback").innerHTML = "Correct!";
+        document.querySelector("#q1Feedback").className = "bg-success text-white";
+        document.querySelector("#markImg1").innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
+        score += 20;
+    } else {
+        document.querySelector("#q1Feedback").innerHTML = "Incorrect!";
+        document.querySelector("#q1Feedback").className = "bg-warning text-white";
+        document.querySelector("#markImg1").innerHTML = "<img src='img/xmark.png' alt='xmark'>";
+    }
+
+    document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
 } // gradeQuiz
