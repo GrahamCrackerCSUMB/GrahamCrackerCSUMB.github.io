8903ac394add71f75bbb93b1a3d6c36ba9d77e8a
ErinH <erhurley@csumb.edu>
2026-03-17 19:06:00 -0700

    trying to make attempts perisitant

--- Homework2/js/script.js
+++ Homework2/js/script.js
@@ -100,5 +100,6 @@ function gradeQuiz(){
 
 document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
 document.querySelector("#totalAttempts").innerHTML = `Total Attempts: ${++attempts}`;
-localStorage.setItem("total_attempts, attempts");
+$("#totalAttempts").html(`Total Attempts: ${++attempts}`);
+localStorage.setItem("total_attempts", attempts);
 } // gradeQuiz
