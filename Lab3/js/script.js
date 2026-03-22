7418b055c63d8c6b681e7d6bb3d1461f8058facc
ErinH <erhurley@csumb.edu>
2026-03-22 15:03:39 -0700

    3.2

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -1,5 +1,6 @@
 //event listeners
 document.querySelector("#zip").addEventListener("change", displayCity);
+document.querySelector("#state").addEventListener("change", displayCounties);
 
 
 //functions
@@ -17,4 +18,18 @@ async function displayCity() {
     document.querySelector("#city").innerHTML = data.city;
     document.querySelector("#longitude").innerHTML = data.longitude;
     document.querySelector("#latitude").innerHTML = data.latitude;
+}
+
+//Displaying counties from Web API based on the two-letter abbreviation of a state
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let countyList = document.querySelector("#county");
+
+    for (let i = 0; i < data.length; i++) {
+        countyList.innerHTML += `<option> ${data[i].county} </option>`;
+    }
 }
