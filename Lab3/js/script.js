a41bfe771e7468e1861ea1e09d3c8fe0df412e7f
ErinH <erhurley@csumb.edu>
2026-03-22 17:36:16 -0700

    fixingg state again

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -112,12 +112,10 @@ async function displayStates() {
     let response = await fetch(url);
     let data = await response.json();
 
-    console.log(data);
-
     let stateList = document.querySelector("#state");
     stateList.innerHTML = "<option value=''>Select One</option>";
 
     for (let i = 0; i < data.length; i++) {
-        stateList.innerHTML += `<option value="${data[i].usps}">${data[i].name}</option>`;
+        stateList.innerHTML += `<option value="${data[i].usps}">${data[i].state}</option>`;
     }
 }
