f26e50e6ff7dd4a28ed95125da214b5f991baa9f
ErinH <erhurley@csumb.edu>
2026-03-23 22:32:36 -0700

    let's add in a second api call to get the weather

--- Homework3/js/script.js
+++ Homework3/js/script.js
@@ -14,17 +14,27 @@ async function getCoordinates() {
         const response = await fetch(url);
         const data = await response.json();
 
-        if (data.error) {
+        if (!data.results) {
             result.innerHTML = "<p class='error'>Invalid ZIP code.</p>";
             return;
         }
 
         const location = data.results[0];
+        const lat = location.latitude;
+        const lon = location.longitude;
+
+        const weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&current_weather=true";
+
+        const weatherResponse = await fetch(weatherUrl);
+        const weatherData = await weatherResponse.json();
+        const temp = weatherData.current_weather.temperature;
+
 
         result.innerHTML =
             "<h3>" + location.name + ", " + location.admin1 + "</h3>" +
-            "<p><strong>Latitude:</strong> " + location.latitude + "</p>" +
-            "<p><strong>Longitude:</strong> " + location.longitude + "</p>";
+            "<p><strong>Latitude:</strong> " + lat + "</p>" +
+            "<p><strong>Longitude:</strong> " + lon + "</p>" +
+            "<p><strong>Temperature:</strong> " + temp + " °C</p>";
 
     } catch (error) {
         result.innerHTML = "<p class='error'>Error!</p>";
